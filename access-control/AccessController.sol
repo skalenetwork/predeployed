ae9b740a05967469a7fa1e9407f4e1f1867e4ef2
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-03-31 18:25:33 +0300

    SKALE-3758 Checking if address is contract

--- access-control/AccessController.sol
+++ access-control/AccessController.sol
@@ -3,22 +3,28 @@ pragma solidity ^0.6.0;
 contract AccessController {
 
     address owner;
-    mapping(address => bool) private whitelisted;
+    mapping(address => bool) private _whitelisted;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
 
-    function addAddress(address addr) external {
-        whitelisted[addr] = true;
+    function addAddress(address addr) external onlyOwner {
+        _whitelisted[addr] = true;
     }
 
-    function removeAddress(address addr) external {
-        whitelisted[addr] = false;
+    function removeAddress(address addr) external onlyOwner {
+        _whitelisted[addr] = false;
     }
 
     function isAddressWhitelisted(address addr) external view returns (bool) {
-        return whitelisted[addr] || addr == owner;
+        return addr == owner || isContract(addr) || _whitelisted[addr];
+    }
+
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly { size := extcodesize(account) }
+        return size > 0;
     }
 }
