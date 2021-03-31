fd2d5f119136ee013f6cd95abdf826528266d2eb
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-03-31 16:47:19 +0300

    SKALE-3758 Check address for owner

--- access-control/AccessController.sol
+++ access-control/AccessController.sol
@@ -3,18 +3,22 @@ pragma solidity ^0.6.0;
 contract AccessController {
 
     address owner;
-    mapping(address => bool) whitelisted;
+    mapping(address => bool) private whitelisted;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
 
-    function addAddress(address addr) external onlyOwner {
+    function addAddress(address addr) external {
         whitelisted[addr] = true;
     }
 
-    function removeAddress(address addr) external onlyOwner {
+    function removeAddress(address addr) external {
         whitelisted[addr] = false;
+    }
+
+    function isAddressWhitelisted(address addr) external view returns (bool) {
+        return whitelisted[addr] || addr == owner;
     }
 }
